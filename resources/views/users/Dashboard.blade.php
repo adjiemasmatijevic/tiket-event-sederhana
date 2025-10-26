cd9fc46e051e30c30707dba8aad1aa96b402c187
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-10-26 21:06:04 +0700

    Add new image

--- resources/views/users/Dashboard.blade.php
+++ resources/views/users/Dashboard.blade.php
@@ -14,39 +14,45 @@
                 <div class="banner-content"><br><br><br><br><br>
                 </div>
             </div>
-        </div>
-        <div class="swiper-pagination style-2"></div>
-    </div>
-</div>
-<!-- Banner End -->
-
-
-<div class="title-bar container mb-0 pb-0">
-    <h5 class="title font-w800 mb-0">Upcoming</h5>
-</div>
-
-<!-- Shop Section Strat -->
-<div class="container">
-    <div class="row g-3">
-        <div class="col-6">
-            @forelse ($upcomingEvents as $event)
-            <a href="{{ route('event_tickets', $event->id) }}">
-                <div class="shop-card">
-                    <div class="dz-media">
-                        <img src="/storage/event_images/{{ $event->image }}.webp" alt="image" />
-                    </div>
-                    <div class="dz-content">
-                        <span class="font-12 text-muted">{{ $event->location }}</span>
-                        <h6 class="title">
-                            <a href="{{ route('event_tickets', $event->id) }}">{{ $event->name }}</a>
-                        </h6>
-                    </div>
+            <div class="swiper-slide">
+                <div class="banner-bg" style="
+                      background-image: url('/pic3.jpg');
+                    "></div>
+                <div class="banner-content"><br><br><br><br><br>
                 </div>
-            </a>
-            @empty
-            <p>No upcoming events available.</p>
-            @endforelse
+            </div>
+            <div class="swiper-pagination style-2"></div>
         </div>
     </div>
-</div>
-@endsection
+    <!-- Banner End -->
+
+
+    <div class="title-bar container mb-0 pb-0">
+        <h5 class="title font-w800 mb-0">Upcoming</h5>
+    </div>
+
+    <!-- Shop Section Strat -->
+    <div class="container">
+        <div class="row g-3">
+            <div class="col-6">
+                @forelse ($upcomingEvents as $event)
+                <a href="{{ route('event_tickets', $event->id) }}">
+                    <div class="shop-card">
+                        <div class="dz-media">
+                            <img src="/storage/event_images/{{ $event->image }}.webp" alt="image" />
+                        </div>
+                        <div class="dz-content">
+                            <span class="font-12 text-muted">{{ $event->location }}</span>
+                            <h6 class="title">
+                                <a href="{{ route('event_tickets', $event->id) }}">{{ $event->name }}</a>
+                            </h6>
+                        </div>
+                    </div>
+                </a>
+                @empty
+                <p>No upcoming events available.</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+    @endsection
